a706e35d86d87262868e08482ce721dbb3e91fb1
Surya Teja Gadde <gaddesuryateja999@gmail.com>
2021-02-08 21:52:01 +0530

    On 8/02/2021

--- SQL/SQLHA3.sql
+++
@@ -1,8 +0,0 @@
-
-insert into student values
-(1,'Ajay','Delhi',90),
-(1,'Ajay','Delhi',90),
-(1,'Ajay','Delhi',90),
-(1,'Ajay','Delhi',90),
-(1,'Ajay','Delhi',90)
-select * from student where rn IN (2,4)
